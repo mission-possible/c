4641fb02ab8199650b66d9abee862c414c70db22
mission-possible <nisha.kumari@iiitb.org>
2017-06-27 00:03:28 +0530

    Update permutations.cpp

--- permutations.cpp
+++ permutations.cpp
@@ -1,22 +1,22 @@
 //generate all possible permutation of a given set
 
-void permute(vector<int> &num, int start, vector<vector<int> > &result) {
-        if (start == num.size() - 1) {
-            result.push_back(num);
-            return;
-        } 
-        for (int i = start; i < num.size(); i++) {
-            swap(num[start], num[i]);
-            permute(num, start + 1, result);
-            swap(num[start], num[i]);
-        }
-    }
+void permute(vector<int> &num, int start, vector<vector<int> > &result){
+    if (start == num.size() - 1){
+        result.push_back(num);
+        return;
+     } 
+     for (int i = start; i < num.size(); i++){
+        swap(num[start], num[i]);
+        permute(num, start + 1, result);
+        swap(num[start], num[i]);
+     }
+}
 
-    vector<vector<int> > permute(vector<int> &num) {
-        vector<vector<int> > result;
-        if (num.size() == 0)
-            return result;
-        sort(num.begin(), num.end());
-        permute(num, 0, result);
+vector<vector<int> > permute(vector<int> &num){
+     vector<vector<int> > result;
+     if (num.size() == 0)
         return result;
-    }
+     sort(num.begin(), num.end());
+     permute(num, 0, result);
+     return result;
+}
